48a040034b76bb55978870bccfdfcfdaa2124a3b
Kaitlin Pet <kaitlin@qastrategy.com>
2018-10-05 15:57:26 -0400

    minimal working gamera_classifier.py

--- gamera_classifier.py
+++ gamera_classifier.py
@@ -4,34 +4,40 @@
 import sys
 # Import the Gamera core and initialize it
 from gamera.core import *
-init_gamera()
-
 # Import the classifier module
 from gamera import knn
-
-def main():
-    # Create a new classifier, figure out feature u like 
-    classifier = knn.kNNNonInteractive([],
-            ["aspect ratio","moments","volume64regions","nrows feature"], 0,normalize=True)
-    # Load training data
-    classifier.from_xml_filename("knn_glyphs.xml")
-
-
-    # Load the image, and convert it to onebit
-    image = load_image(sys.argv[-1])
-    onebit = image.to_onebit()
-
-    # Get the connected components from the image
-    component_list = onebit.cc_analysis()
-    
-    #classify images choose grouping function
-    classifiedimgs = classifier.group_and_update_list_automatic(component_list, \
-            ShapedGroupingFunction(STAFF_HEIGHT), max_parts_per_group=2)
-
-    # Display the ccs to show their classification
-    display_multi(classifiedimgs)
+from gamera import classify
+from gamera import gamera_xml
+init_gamera()
 
 
 
 
-main()
+# Create a new classifier, figure out feature u like 
+classifier = knn.kNNNonInteractive("knn_glyphs.xml",
+        ["aspect_ratio","moments","volume64regions","nrows_feature"], 0,normalize=True)
+# Load training data
+#classifier.from_xml_filename("knn_glyphs.xml")
+
+
+# Load the image, and convert it to onebit
+image = load_image(sys.argv[-1])
+STAFF_HEIGHT=2 #int(sys.argv[2])
+onebit = image.to_onebit()
+
+# Get the connected components from the image
+component_list = onebit.cc_analysis()
+
+#classify images choose grouping function
+classifiedimgs = classifier.group_and_update_list_automatic(component_list, \
+  #      grouping_function=BoundingBoxGroupingFunction(2),\
+        max_parts_per_group=2)
+
+# Display the ccs to show their classification
+#display_multi(classifiedimgs)
+
+#Inspect performance in GUI
+gamera_xml.glyphs_to_xml("knn-results.xml", classifiedimgs, False)
+
+
+
