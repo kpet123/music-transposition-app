287a5eed4a750c6a8fab0d8a743634f4fbdca863
Kaitlin Pet <kaitlin@qastrategy.com>
2018-11-23 19:37:05 -0500

    gamera_getCC.py removes system, still need to change size parameters for system so more universally applicable

--- gamera_getCC.py
+++ gamera_getCC.py
@@ -21,17 +21,29 @@ component_list = onebit.cc_analysis()
 
 print len(component_list)
 
-system_list = []
+system_list = [] #list with all systems
+
+#generate system_list
 for c in component_list:
     size = c.ncols*c.nrows 
-
     #find systems, rule out by length and size
     if size > onebit.ncols*20  and c.ncols > .6*onebit.ncols:
-        arr = c.to_numpy()/c.label #1s and 0s, 1 is black
-        print "coordinate of cc is "+str(c.offset_x)+","+str(c.offset_y)
+        copy = c.image_copy()
+        
+        #do rough staffline extraction
+        proj_arr = copy.run_histogram("black", "vertical")
+        most_frequent_occurences = max(proj_arr)
+        value = proj_arr.index(most_frequent_occurences)
+        copy.filter_short_runs(value-1, "black")
+        copy.filter_tall_runs(value+1, "black")
+        
+        #convert to array and add to system_list
+        arr = copy.to_numpy()/c.label
         system_list.append({"arr": arr, "offset_x": c.offset_x, \
                 "offset_y": c.offset_y})
+ 
+
 with open("systems.pkl", "w") as f : 
-        cPickle.dump(system_list, f, cPickle.HIGHEST_PROTOCOL)
+    cPickle.dump(system_list, f, cPickle.HIGHEST_PROTOCOL)
 
 
